38a23fde0848a7ba03697e597a22c44d822b2a2b
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2021-07-13 12:40:47 +0200

    Basic working system

--- src/Server/src/server.js
+++ src/Server/src/server.js
@@ -1,14 +1,43 @@
+const { validate: uuidValidate } = require('uuid');
+
 const express = require('express');
 const app = express();
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    helpers: {
+        year: function() { return new Date().getFullYear(); }
+    }
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 app.get('/payment/:otc', (req, res) => {
-    res.send('Payment OTC ' + req.params.otc);
+    if(!uuidValidate(req.params.otc)) {
+        res.status(400).end();
+        return;
+    }
+
+    console.log('Payment ' + req.params.otc);
+
+    res.render('payment', {
+        otc: req.params.otc
+    });
 });
 
 app.get('/vouchers/:otc', (req, res) => {
-    res.send('Vouchers OTC ' + req.params.otc);
+    if(!uuidValidate(req.params.otc)) {
+        res.status(400).end();
+        return;
+    }
+
+    console.log('Vouchers ' + req.params.otc);
+
+    res.render('vouchers', {
+        otc: req.params.otc
+    });
 });
 
 const listener = app.listen(process.env.PORT, function () {
